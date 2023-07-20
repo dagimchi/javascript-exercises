76d5a372c0ccf09e0715327fb079084eb2b0a4e8
Hae <haechoi92@yahoo.com>
2023-07-19 17:17:06 -0700

    complete exercises js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (start > end) {
+    for (let i = start; i >= end; i--) {
+      sum += i;
+    }
+    return sum;
+  } else if (start < 0 || end < 0) return "ERROR";
+  else if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let celsius = (5 / 9) * (f - 32);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let fah = c * (9 / 5) + 32;
+  return Math.round(fah * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
